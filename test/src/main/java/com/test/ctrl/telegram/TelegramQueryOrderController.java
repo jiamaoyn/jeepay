82809edd8b37debe441cdc8eacf7171ffbdee2ba
jiamao.wang <jiamao.wang@shgic.com>
2023-12-05 12:15:21 +0800

    测试模块

--- test/src/main/java/com/test/ctrl/telegram/TelegramQueryOrderController.java
+++ test/src/main/java/com/test/ctrl/telegram/TelegramQueryOrderController.java
@@ -6,12 +6,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
-@RequestMapping("/api/telegram")
+@RequestMapping("/api/ceshi")
 public class TelegramQueryOrderController {
-    @RequestMapping(value = "/orderNch/{mchOrderNo}/{channelOrderNo}")
-    private ApiRes<Object> toPayForm(@PathVariable("mchOrderNo") String mchOrderNo, @PathVariable("channelOrderNo") String channelOrderNo) {
-        return ApiRes.ok("不是商家账单，无法回调");
+    @RequestMapping(value = "/info")
+    private ApiRes<Object> toPayForm(HttpServletRequest request) {
+        System.out.println(request.getRequestURI());
+        System.out.println(request.getScheme());
+        System.out.println(request.getServerName());
+        String realIp = request.getHeader("X-Real-IP");
+        String forwardedFor = request.getHeader("X-Forwarded-For");
+        String protocol = request.getHeader("X-Forwarded-Proto");
+        String Host = request.getHeader("Host");
+        System.out.println(realIp + "---------" +forwardedFor + "---------"+protocol+ "---------"+Host);
+        return ApiRes.ok(request);
     }
 }
